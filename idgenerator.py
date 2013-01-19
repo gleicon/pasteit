a6e035e2cac7b9835d961e9918e215ef35d7b390
Gleicon Moraes <gleicon@gmail.com>
2013-01-19 18:10:54 -0200

    counter fix

--- idgenerator.py
+++ idgenerator.py
@@ -1,11 +1,12 @@
 # uuid generator
 import redis
 
+
 class IdGenerator():
-    def __init__(self, name = None):
+    def __init__(self, name="generic"):
         p = redis.ConnectionPool(host='localhost', port=6379, db=0)
-        self._redis = redis.Redis(connection_pool = p)
-        self._countername = "%s:counter" % "generic" if not name else name
+        self._redis = redis.Redis(connection_pool=p)
+        self._countername = "%s:counter" % name
 
     def request(self):
         return self._redis.incr(self._countername)
